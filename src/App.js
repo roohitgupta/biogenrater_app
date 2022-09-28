161404a0acbc82076f325034f1f33484472db14b
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-09-29 01:18:51 +0530

    build basic UI sections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BioForm from "./components/BioForm";
+import ResultBox from "./components/ResultBox";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="w-full border-solid border-4 border-red-500">
+        <h1 className="text-center text-xl p-3 font-bold">
+          Boi Generator
+        </h1>
+      </div>
+
+      <div className="flex">
+        <BioForm />
+        <ResultBox />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/BioForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BioForm = () => {
+  return (
+    <div className='w-1/2 h-96 m-3 border-solid border-4 border-sky-500'>
+      <h1 className='text-xl text-center font-bold underline'>Bio Form</h1>
+    </div>
+  )
+}
+
+export default BioForm
