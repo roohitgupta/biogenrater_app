dc3c52958cd5352219de5fa94d5beda7e4f30f51
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-09-30 19:49:20 +0530

    form UI layout complete

--- src/components/BioForm.js
+++ src/components/BioForm.js
@@ -1,47 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BioForm = () => {
-  return (
-    <div className="flex flex-col items-center border-solid border-4 border-pink-500 lg:flex-row justify-center">
-      <div className="w-2/3 h-96 m-3 border-solid border-4 border-sky-500 lg:w-1/2">
-        <h1 className="text-xl text-center font-bold underline">Bio Form</h1>
+  const [name, setName] = useState("Rohit Gupta");
+  const [gender, setGender] = useState("Male");
 
-        <div className="flex justify-between items-center bg-gray-200 my-3 w-full h-30 border-solid border-2 border-red-300 p-3">
-          <div className="w-1/4 h-full flex">
+  return (
+    <div className="flex flex-col items-center lg:flex-row justify-center">
+      <div className="w-2/3 h-full m-3 border-solid border-4 border-sky-500 lg:w-1/2">
+        <h1 className="text-xl text-center font-bold underline">Bio Form</h1>
+        {/* first line of form =================*/}
+        <div className="flex justify-between items-center text-sm bg-gray-200 my-3 w-full h-30  p-3">
+          <div className="flex">
             <label htmlFor="">Name</label>
             <input
-              className="p-1.5 mx-2 border rounded-md shadow-sm"
+              className="p-1.5 mx-2 rounded-md shadow-sm"
               type="text"
               placeholder="Full Name"
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
-          <div className="w-1/4 h-full flex">
+          <div className="flex">
             <label htmlFor="">Gender</label>
-            <select className="w-full mx-2 p-1.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+            <select
+              className="w-full mx-2 p-1.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+              onChange={(e) => setGender(e.target.value)}
+            >
               <option>Male</option>
               <option>Female</option>
             </select>
           </div>
-          <div className="w-1/4 h-full">
+          <div className="">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
               Random Name
             </button>
           </div>
         </div>
-
-        <div className="flex justify-between bg-gray-200 my-3 w-full h-30 border-solid border-2 border-red-300 p-3">
+        {/* second line of form ===========*/}
+        <div className="flex justify-between items-center text-sm bg-gray-200 my-3 w-full h-30 p-3">
           <div className="form-check">
-            <input
-              className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label
-              className="form-check-label inline-block text-gray-800"
-              for="flexCheckDefault"
-            >
-              Location
+            <label class="inline-flex items-center">
+              <input
+                type="checkbox"
+                class="form-checkbox h-5 w-5 text-gray-600"
+              />
+              <span class="ml-2 text-gray-700">Location</span>
             </label>
             <input
               className="p-1.5 mx-2 border rounded-md shadow-sm"
@@ -49,19 +51,144 @@ const BioForm = () => {
               placeholder="Location"
             />
           </div>
-          <div className="w-1/4 h-full">
+          <div className="h-full">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
               Random Location
             </button>
           </div>
         </div>
+        {/* third line of form ============ */}
+        <div className="flex flex-col bg-gray-200 text-sm my-3 w-full h-30 p-3">
+          <div className="flex justify-between items-center mb-2">
+            <div className="form-check">
+              <label class="inline-flex items-center">
+                <input
+                  type="checkbox"
+                  class="form-checkbox h-5 w-5 text-gray-600"
+                />
+                <span class="ml-2 text-gray-700">School</span>
+              </label>
+              <input
+                className="p-1.5 mx-2 border rounded-md shadow-sm"
+                type="text"
+                placeholder="School"
+              />
+            </div>
+            <div className="h-full">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+                Random School
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex">
+              <label htmlFor="">Major</label>
+              <input
+                className="p-1.5 mx-2 rounded-md shadow-sm"
+                type="text"
+                placeholder="Major"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <button className="bg-blue-500 hover:bg-blue-700  text-white font-bold py-1 px-2 rounded">
+                Random Major
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* fourth line of form */}
+        <div className="flex justify-between items-center mb-2 bg-gray-200 text-sm my-3 w-full h-30 p-3">
+          <div className="form-check">
+            <label class="inline-flex items-center">
+              <input
+                type="checkbox"
+                class="form-checkbox h-5 w-5 text-gray-600"
+              />
+              <span class="ml-2 text-gray-700">Occupation</span>
+            </label>
+            <input
+              className="p-1.5 mx-2 border rounded-md shadow-sm"
+              type="text"
+              placeholder="Occupation"
+            />
+          </div>
+          <div className="h-full">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+              Random Occupation
+            </button>
+          </div>
+        </div>
+        {/* fifth line of form */}
+        <div className="flex justify-between flex-col  mb-2 bg-gray-200 text-sm my-3 w-full h-30 p-3">
+          <div className="form-check">
+            <label class="inline-flex items-center">
+              <input
+                type="checkbox"
+                class="form-checkbox h-5 w-5 text-gray-600"
+              />
+              <span class="ml-2 text-gray-700">Religious Background</span>
+            </label>
+            <div>
+              <textarea
+                className="p-1.5 w-full text-base mx-2 border rounded-md shadow-sm"
+                rows="4"
+                type="text"
+                placeholder="Occupation"
+              ></textarea>
+            </div>
+          </div>
+          <div className="h-full">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+              Random Occupation
+            </button>
+          </div>
+        </div>
+        {/* sixth line of form */}
+        <div className="flex justify-between flex-col  mb-2 bg-gray-200 text-sm my-3 w-full h-30 p-3">
+          <div className="form-check">
+            <label class="inline-flex items-center">
+              <input
+                type="checkbox"
+                class="form-checkbox h-5 w-5 text-gray-600"
+              />
+              <span class="ml-2 text-gray-700">
+                Reason for meeting with missionaries
+              </span>
+            </label>
+            <div>
+              <textarea
+                className="p-1.5 w-full text-base mx-2 border rounded-md shadow-sm"
+                rows="4"
+                type="text"
+                placeholder="Occupation"
+              ></textarea>
+            </div>
+          </div>
+          <div className="h-full">
+            <button className="bg-pink-400 hover:bg-blue-700 text-white font-bold py-1 mx-2 px-2 rounded">
+              Restoration
+            </button>
+            <button className="bg-green-500 hover:bg-blue-700 text-white font-bold py-1 mx-2 px-2 rounded">
+              Plan of Salvation
+            </button>
+            <button className="bg-purple-400 hover:bg-blue-700 text-white font-bold py-1 mx-2 px-2 rounded">
+              Gospel of Christ
+            </button>
+            <button className="bg-orange-400 hover:bg-blue-700 text-white font-bold py-1 mx-2 px-2 rounded">
+              Law of Chastity
+            </button>
+          </div>
+        </div>
       </div>
-
-
-      <div className='w-1/2 h-64 m-3 border-solid border-4 border-sky-500 lg:w-1/3'>
-      <h1 className='text-xl text-center font-bold underline'>Result Box</h1>
-    </div>
-
+      {/* Result box */}
+      <div className="w-1/2 h-64 m-3 border-solid border-4 border-sky-500 lg:w-1/3">
+        <h1 className="text-xl text-center font-bold underline">Result Box</h1>
+        <h3>
+          My name is {name}. and I'm {gender}
+        </h3>
+      </div>
     </div>
   );
 };
